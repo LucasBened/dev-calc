d330501d22a0b9fcffbeb4efd6f0bc25383b82f7
Lucas da Silva <100500063+LucasBened@users.noreply.github.com>
2023-12-15 17:16:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,3 @@ button.addEventListener('click', () =>{
   result.textContent  = "O valor que devera ser cobrado é de: " + Number((iptHour.value * iptExt.value) + Number(taxs.value)).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + " R$"
 
 })
-// Nesse caso, estou passando a opção pt-BR para a função toLocaleString(), indicando que desejo formatar o número de acordo com a localidade brasileira. Também estou passando as opções minimumFractionDigits e maximumFractionDigits para indicar o número mínimo e máximo de casas decimais a serem exibidas.   
